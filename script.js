985ca1738243a5984da17db013db6dcd3b526703
ciliescu97 <146425513+ciliescu97@users.noreply.github.com>
2023-11-04 12:22:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-let slideIndex = 0;
+let slideIndex = 1;
 showSlides();
 
+function plusSlides(n) {
+  showSlide(slideIndex += n);
+}
+
 function showSlides() {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+function showSlide(n) {
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // NAVBAR
@@ -33,4 +72,3 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
